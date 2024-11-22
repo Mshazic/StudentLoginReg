bf63a1333a02148ad59a9dec982cf40e8e6900b4
senzonzuza <senzokaqhaka@gmail.com>
2024-11-22 12:13:43 +0200

    Login view done Css added

---
+++ ViewModel/VerifyViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentLoginReg.ViewModel
+{
+	public class VerifyViewModel
+	{
+
+        [Required(ErrorMessage = "Name is required")]
+        [EmailAddress]
+        public string? Email { get; set; }
+    }
+}
+
