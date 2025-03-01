655444ac6db43c91b941eb028694a83bac9cea6b
senzonzuza <senzokaqhaka@gmail.com>
2025-03-01 08:48:49 +0200

    views done and viewModel

---
+++ ViewModel/StudentManagement/AddStdManagementViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace StudentLoginReg.ViewModel.StudentManagement
+{
+	public class AddStdManagementViewModel
+	{
+
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool Subscribed { get; set; }
+
+    }
+}
+
